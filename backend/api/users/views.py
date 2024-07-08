b8e03b1abf33ea6b191d39170c5ec634153bdc22
Meleshin Stepan <mestepanik@gmail.com>
2024-07-08 23:42:05 +0300

    Pretty shit.

--- backend/api/users/views.py
+++ backend/api/users/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
+
 from users.models import Subscription
 
 from .serializers import UserSerializer
